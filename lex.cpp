ccbc824a917e6e3bd27911f3f09af26b8778f656
anenokil <anenokil.w@gmail.com>
2024-04-14 02:08:07 +0300

    lex.cpp: style

--- lex.cpp
+++ lex.cpp
@@ -1,5 +1,5 @@
-#include <cstring> // strchr()
 #include "lex.h"
+#include <cstring> // strchr()
 
 const char ESCAPEBLE_CHARS[] = "\\nt\""; // chars that may appear after the escape char
 const char SPEC_BEG_CHARS[] = "=!<>"; // chars that double special lexemes may start with
@@ -16,39 +16,39 @@ enum flag_vals {
     ERROR,
 };
 
-bool is_point(char c)
+bool _is_point(char c)
 {
     return c == '.';
 }
 
-bool is_quot(char c)
+bool _is_quot(char c)
 {
     return c == '"';
 }
 
-bool is_escape(char c)
+bool _is_escape(char c)
 {
     return c == '\\';
 }
 
-bool is_escapeble(char c)
+bool _is_escapeble(char c)
 {
     return strchr(ESCAPEBLE_CHARS, c);
 }
 
-bool is_spec_beg(char c)
+bool _is_spec_beg(char c)
 {
     return strchr(SPEC_BEG_CHARS, c);
 }
 
-bool is_spec_end(char c)
+bool _is_spec_end(char c)
 {
     return strchr(SPEC_END_CHARS, c);
 }
 
 bool is_num_quant(char c)
 {
-    return std::isdigit(c) || is_point(c);
+    return std::isdigit(c) || _is_point(c);
 }
 
 bool is_name_quant(char c)
@@ -56,38 +56,38 @@ bool is_name_quant(char c)
     return std::isalpha(c) || c == '_';
 }
 
-flag_vals get_flag(char c)
+flag_vals _get_flag(char c)
 {
     if (is_name_quant(c)) {
         return NAME;
     } else if (is_num_quant(c)) {
         return NUM;
-    } else if (is_quot(c)) {
+    } else if (_is_quot(c)) {
         return TEXT;
-    } else if (is_spec_beg(c)) {
+    } else if (_is_spec_beg(c)) {
         return SPEC;
-    } else if (is_spec_end(c)) {
+    } else if (_is_spec_end(c)) {
         return SPEC_END;
     } else {
         return ERROR;
     }
 }
 
-bool has_correct_flag(char c, flag_vals & flag)
+bool _has_correct_flag(char c, flag_vals & flag)
 {
     if (flag == NAME) {
         return is_name_quant(c);
     } else if (flag == NUM) {
         return is_num_quant(c);
     } else if (flag == TEXT) {
-        if (is_escape(c)) {
+        if (_is_escape(c)) {
             flag = TEXT_ESCAPE;
-        } else if (is_quot(c)) {
+        } else if (_is_quot(c)) {
             flag = TEXT_END;
         }
         return true;
     } else if (flag == TEXT_ESCAPE) {
-        if (is_escapeble(c)) {
+        if (_is_escapeble(c)) {
             flag = TEXT;
             return true;
         }
@@ -97,7 +97,7 @@ bool has_correct_flag(char c, flag_vals & flag)
         return false;
     } else if (flag == SPEC) {
         flag = SPEC_END;
-        return is_spec_end(c);
+        return _is_spec_end(c);
     } else if (flag == SPEC_END) {
         return false;
     }
@@ -118,7 +118,7 @@ std::string get_lex(std::ifstream & ifs, ret_vals & ret)
     }
 
     /* read the first non-space character */
-    flag_vals flag = get_flag(c);
+    flag_vals flag = _get_flag(c);
     if (flag == ERROR) {
         ret = RET_ERR;
         return "";
@@ -129,7 +129,7 @@ std::string get_lex(std::ifstream & ifs, ret_vals & ret)
     do {
         res += c;
         c = ifs.get();
-    } while (!ifs.eof() && has_correct_flag(c, flag));
+    } while (!ifs.eof() && _has_correct_flag(c, flag));
     if (flag == ERROR) {
         ret = RET_ERR;
         return "";
