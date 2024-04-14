99937210bdce00bbc7882678986411b87ba7e7f8
anenokil <anenokil.w@gmail.com>
2024-04-14 22:54:26 +0300

    Implement lexeme classification function (define_type)

--- lex.cpp
+++ lex.cpp
@@ -98,3 +98,60 @@ std::string get_lex(std::ifstream & ifs, ret_vals & ret)
     ret = RET_OK;
     return res;
 }
+
+bool _is_var_name(std::string const & lex)
+{
+    for (char c: lex) {
+        if (!is_name_quant(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool _is_num_const(std::string const & lex)
+{
+    bool has_point = false;
+    for (char c: lex) {
+        if (is_point(c)) {
+            if (has_point) {
+                return false;
+            }
+            has_point = true;
+        } if (!std::isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool _is_str_const(std::string const & lex)
+{
+    return lex.length() >= 2 && lex[0] == '"' && lex[lex.length() - 1] == '"';
+}
+
+lex_types define_type(std::string const & lex)
+{
+    if (lex == "") return LEX_UNKNOWN;
+    if (lex == "program") return LEX_ROOT;
+    if (lex == "+" || lex == "-" || lex == "*" || lex == "/" || lex == "%" || lex == "~" || lex == "==" || 
+        lex == "!=" || lex == ">" || lex == ">=" || lex == "<" || lex == "<=") return LEX_OPER_NORET;
+    if (lex == "=") return LEX_OPER_RET;
+    if (lex == "int" || lex == "float" || lex == "bool" || lex == "str") return LEX_TYPE;
+    if (lex == "if") return LEX_IF;
+    if (lex == "else") return LEX_ELSE;
+    if (lex == "for") return LEX_FOR;
+    if (lex == "while") return LEX_WHILE;
+    if (lex == "do") return LEX_DO;
+    if (lex == "until") return LEX_UNTIL;
+    if (lex == "{") return LEX_SCOPE_L;
+    if (lex == "}") return LEX_SCOPE_R;
+    if (lex == "(") return LEX_PARENTHESIS_L;
+    if (lex == ")") return LEX_PARENTHESIS_R;
+    if (lex == "break" || lex == "continue" || lex == "read" || lex == "write") return LEX_CMD;
+    if (_is_var_name(lex)) return LEX_VAR;
+    if (lex == "true" || lex == "false" || _is_num_const(lex) || _is_str_const(lex)) return LEX_CONST;
+    if (lex == ",") return LEX_COMMA;
+    if (lex == ";") return LEX_END;
+    return LEX_UNKNOWN;
+}
--- lex.h
+++ lex.h
@@ -10,8 +10,34 @@ enum ret_vals {
     RET_EOF,
 };
 
+enum lex_types {
+    LEX_ROOT,
+    LEX_OPER_NORET,
+    LEX_OPER_RET,
+    LEX_TYPE,
+    LEX_IF,
+    LEX_ELSE,
+    LEX_FOR,
+    LEX_WHILE,
+    LEX_DO,
+    LEX_UNTIL,
+    LEX_SCOPE_L,
+    LEX_SCOPE_R,
+    LEX_PARENTHESIS_L,
+    LEX_PARENTHESIS_R,
+    LEX_CMD,
+    LEX_VAR,
+    LEX_CONST,
+    LEX_COMMA,
+    LEX_END,
+    LEX_UNKNOWN,
+};
+
 /* the function gets one lexeme from file and returns it;
  * sets 'ret' value */
 std::string get_lex(std::ifstream & ifs, ret_vals & ret);
+
+/* the function receives a lexeme and returns its type */
+lex_types define_type(std::string const & lex);
 
 #endif
